4cd4c0071bb4c73c94acfe193f71ce654e8cc4f7
unknown <ragavaabhishek55@gmail.com>
2024-04-21 00:49:50 +0530

    working on css

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-fetch('http://localhost:3000/products')
-.then(res => res.json())
-.then(data => displayData(data))
-.catch(error => console.error(error));
+function fetchProducts() {
+    fetch('http://localhost:3000/products')
+    .then(res => res.json())
+    .then(data => displayData(data))
+    .catch(error => console.error(error));
+}
+
+window.addEventListener('load', () => {
+    fetchProducts();
+});
+
+// Add event listener for category filter
+// Function to fetch products based on category and price sorting
+const categoryFilterElement = document.getElementById("category-filter");
+categoryFilterElement.addEventListener('change', function() {
+    const selectedCategory = this.value;
+    fetchProductsByCategory(selectedCategory);
+});
+
+// Add event listener for sorting
+const sortSelectElement = document.getElementById("sort-select");
+sortSelectElement.addEventListener('change', function() {
+    const selectedSortOption = this.value;
+    const selectedCategory = categoryFilterElement.value;
+    fetchProductsByCategory(selectedCategory, selectedSortOption);
+});
+
+
+function fetchProductsByCategory(category) {
+    let url = 'http://localhost:3000/products';
+    if (category !== 'all') {
+        url += `?category=${category}`;
+    }
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayData(data))
+    .catch(error => console.error(error));
+}
 
 function displayData(products){
     const productList = document.getElementById("product-list");
+    productList.innerHTML = ''; // Clear previous products
+
     products.forEach(product => {
-        const words = product.description.split(' ');
-        const shortDesc = words.slice(0, 15).join(' ');
+        const word = product.description.split('');
+        const shortDesc = word.slice(0, 15).join('');
+
         const productCard = `
             <div class="card">
                 <img src="${product.thumbnail}" alt="${product.name}">
@@ -20,49 +58,12 @@ function displayData(products){
                 </div>
             </div>
         `;
-        productList.insertAdjacentHTML('beforeend', productCard);
+        productList.innerHTML += productCard;
     });
 }
 
-const urlParams = new URLSearchParams(window.location.search);
-const proIdtype = urlParams.get('id');
-const productId = parseInt(proIdtype, 10); // Parse as integer with base 10
-console.log(typeof(productId));
-console.log(productId);
 
-if(productId){
-    fetch(`http://localhost:3000/products/${productId}`)
-    .then(res => res.json())
-    .then(product => {
-        displayDetails(product);
-    })
-    .catch(error => console.error(error));
-}
 
-function displayDetails(product){
-    const productDetail = document.getElementById("product-detail");
 
-    const imagesHTML = product.images.map(image => {
-        if (image) {
-            return `<img src="${image}" alt="${product.name}">`;
-        } else {
-            return '';
-        }
-    }).join('');
 
-    const productDetailHTML = `
-        <div class="card">
-        <img src="${product.thumbnail}" alt="${product.name}">
-            <div class="card-body">
-                <h3>${product.name}</h3>
-                <p>${product.price}</p>
-                <p>${product.category}</p> 
-                <p>${product.description}</p> 
-                <div class="product-images">
-                ${imagesHTML}
-            </div>
-            </div>
-        </div>
-    `;
-    productDetail.innerHTML = productDetailHTML;
-}
+
