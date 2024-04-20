64929d687154c2687fa6193f7f842ebdd11781cc
unknown <ragavaabhishek55@gmail.com>
2024-04-21 00:14:30 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+fetch('http://localhost:3000/products')
+.then(res => res.json())
+.then(data => displayData(data))
+.catch(error => console.error(error));
+
+function displayData(products){
+    const productList = document.getElementById("product-list");
+    products.forEach(product => {
+        const words = product.description.split(' ');
+        const shortDesc = words.slice(0, 15).join(' ');
+        const productCard = `
+            <div class="card">
+                <img src="${product.thumbnail}" alt="${product.name}">
+                <div class="card-body">
+                    <h3>${product.name}</h3>
+                    <p>${product.price}</p>
+                    <p>${product.category}</p> 
+                    <p>${shortDesc}</p> 
+                    <a href="product.html?id=${product.id}">View Details</a>
+                </div>
+            </div>
+        `;
+        productList.insertAdjacentHTML('beforeend', productCard);
+    });
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const proIdtype = urlParams.get('id');
+const productId = parseInt(proIdtype, 10); // Parse as integer with base 10
+console.log(typeof(productId));
+console.log(productId);
+
+if(productId){
+    fetch(`http://localhost:3000/products/${productId}`)
+    .then(res => res.json())
+    .then(product => {
+        displayDetails(product);
+    })
+    .catch(error => console.error(error));
+}
+
+function displayDetails(product){
+    const productDetail = document.getElementById("product-detail");
+
+    const imagesHTML = product.images.map(image => {
+        if (image) {
+            return `<img src="${image}" alt="${product.name}">`;
+        } else {
+            return '';
+        }
+    }).join('');
+
+    const productDetailHTML = `
+        <div class="card">
+        <img src="${product.thumbnail}" alt="${product.name}">
+            <div class="card-body">
+                <h3>${product.name}</h3>
+                <p>${product.price}</p>
+                <p>${product.category}</p> 
+                <p>${product.description}</p> 
+                <div class="product-images">
+                ${imagesHTML}
+            </div>
+            </div>
+        </div>
+    `;
+    productDetail.innerHTML = productDetailHTML;
+}
